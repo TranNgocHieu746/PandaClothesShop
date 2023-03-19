3427a36012060cf477072c20d8ee6998167161f0
HieuTran <tranngochieu746@gmail.com>
2023-03-19 23:38:05 +0700

    update cart, update variation

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+
 
 
 # Create your views here.
@@ -13,8 +14,21 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    # get the product
+     # get the product
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
+   
     try:
         # get the cart using the cart_id present in the session
         cart = Cart.objects.get(cart_id=_cart_id(request))
